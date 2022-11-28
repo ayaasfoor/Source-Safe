6135f06dccbe746958c16cf527e21741f0b0447c
ayaasfoor <aya.asfour.it@gmail.com>
2022-11-28 11:06:19 +0200

    add  lll

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+llll
 
 //TODO::File mangement
 Route::resource('files', FileController::class);
